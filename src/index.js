6176f756a7f369d9389560645fb288870ff460a5
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-14 17:56:45 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -82,6 +82,39 @@ function jsFunction(value)
 switch (expr) {
   case '0':
     console.log('Oranges are $0.59 a pound.');
+
+function fetchdogss(){
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(resp => resp.json())
+  .then(datab => renderul(datab));
+}
+function changecolor(b) {
+
+  let oo=document.querySelectorAll("li");
+      console.log(oo.length);
+
+   for (let ino in oo) {
+  //document.addEventListener("DOMContentLoaded", function() {
+
+  oo[ino].addEventListener("click", function() {
+      //  console.log("b");
+
+   oo[ino].style.color= "red";
+
+ });}}
+function renderul(b) {
+    const ddd = document.getElementById("dog-breeds");
+    for (const p in b.message) {
+      if(p.startWith("a")){
+    const l = document.createElement('li');
+    l.innerHTML =p;
+    ddd.appendChild(l);
+  }}
+  changecolor(b);
+}
+fetchdogss();*/
+    
+    
     break;
   case '1':
   case '2':
