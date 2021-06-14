4d1ed18adfdf7022b627fbf281e49c94a6cc896f
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-14 17:51:01 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -78,5 +78,18 @@ fetchdogss();
 
 function jsFunction(value)
 {
-    alert(value);
+  const expr = document.getElementById("breed-dropdown").selectedIndex;
+switch (expr) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+  case 'Mangoes':
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    // expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
+    alert( document.getElementById("breed-dropdown").selectedIndex;);
 }
