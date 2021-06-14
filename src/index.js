951d9facbe2b43820a8e91794d752851eff4573e
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-14 16:54:23 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -74,49 +74,6 @@ fetchdogss();
 
 //challenge4
 
-function fetchdogssss()
-{
-  fetch('https://dog.ceo/api/breeds/list/all')
-  .then(resp => resp.json())
-  .then(datab => yaa(datab));
-}
-function order(b){
-  renderul(b);
-let x = document.getElementById("breed-dropdown");
-let list = x.options[x.selectedIndex].value;
-const dddd = document.getElementById("dog-breeds");
-for (const p in b.message) {
-  if(p.charAt(0)===list){
-
-const y= document.createElement('li');
-y.innerHTML =p;
-dddd.appendChild(y);}
-// switch (list){
-//   case "a":console.log("a") ;break;
-//   case "b":console.log("ya");break;
-//   case "c":console.log("ya");break;
-//   case "d":console.log("ya");break;
-//   default:break;
-//
-// }
-//b.message.filter(item=>item.startWith(list))
-
-
-
-}}
-function  yaa(){
-  let x = document.getElementById("breed-dropdown");
-  let list = x.options[x.selectedIndex].value;
-  updateBreedList(breeds.filter(breed => breed.startsWith(list)));
-
-  let breedDropdown = document.querySelector('#breed-dropdown');
-    breedDropdown.addEventListener('change', function (event) {
-      selectBreedsStartingWith(event.target.value);
-    });
-}
-fetchdogssss();
-
-
 var x = document.getElementById("breed-dropdown").selectedIndex;
 var y = document.getElementById("breed-dropdown").options;
 console.log(x)
